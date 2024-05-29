b9a5961612ae5735029231e39828d862ed428f17
JsMares <JsMares0105@gmail.com>
2024-05-28 18:17:28 -0600

    Design Log In UI -Create layout with fields for email and password. -Add a button to validate user's information (not working yet). -Add a text button to navigate to the register screen (not working yet)

--- app/src/main/java/com/example/login_firebase/views/LoginView.kt
+++ app/src/main/java/com/example/login_firebase/views/LoginView.kt
@@ -3,26 +3,37 @@ package com.example.login_firebase.views
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.login_firebase.R
 
 @Preview(showBackground = true)
 @Composable
@@ -30,6 +41,7 @@ fun LoginScreen() {
     Column(modifier = Modifier.fillMaxSize()) {
         HeaderLogin(modifier = Modifier.weight(1f))
         BodyLogin(modifier = Modifier.weight(2f))
+        TailLogin(modifier = Modifier.weight(1f))
     }
 }
 
@@ -56,6 +68,16 @@ fun BodyLogin(modifier: Modifier) {
     Column(modifier = modifier.fillMaxSize()) {
         HeaderLoginText(Modifier.align(Alignment.CenterHorizontally))
         EmailTextField()
+        PasswordTextField()
+        ShowPasswordCheckBox()
+    }
+}
+
+@Composable
+fun TailLogin(modifier: Modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
+        LogInButton(modifier = Modifier.align(Alignment.Center))
+        RegisterIndicationText(modifier = Modifier.align(Alignment.BottomCenter))
     }
 }
 
@@ -74,8 +96,69 @@ fun EmailTextField() {
     OutlinedTextField(value = "", onValueChange = { }, modifier = Modifier
         .fillMaxWidth()
         .padding(start = 12.dp, end = 12.dp, top = 25.dp),
-        label = { Text(text = "Correo Electronico") },
+        label = { Text(text = stringResource(id = R.string.email)) },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
         leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "Email icon") }
     )
 }
+
+@Composable
+fun PasswordTextField() {
+    OutlinedTextField(
+        value = "",
+        onValueChange = { },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 12.dp, end = 12.dp, top = 20.dp),
+        label = { Text(text = stringResource(id = R.string.password)) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "Lock icon") }
+    )
+}
+
+@Composable
+fun ShowPasswordCheckBox() {
+    val checkedState = remember { mutableStateOf(false) }
+
+    Row {
+        Checkbox(checked = checkedState.value, onCheckedChange = { checkedState.value = it })
+        Text(
+            text = stringResource(id = R.string.showPassword),
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+    }
+}
+
+@Composable
+fun LogInButton(modifier: Modifier) {
+    Button(
+        onClick = { }, modifier = modifier
+            .fillMaxWidth()
+            .padding(start = 12.dp, end = 12.dp),
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(Color.Black)
+    ) {
+        Text(text = stringResource(id = R.string.logInButton))
+    }
+}
+
+@Composable
+fun RegisterIndicationText(modifier: Modifier) {
+    Row(
+        modifier = modifier.padding(bottom = 20.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.IndicationRegisterQuestion),
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+        TextButton(onClick = {  }) {
+            Text(
+                text = stringResource(id = R.string.register),
+                color = Color.Black,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+
